4da84ae442073aa2771fc83b827b1e28df084b01
jamesjclark1 <74678757+jamesjclark1@users.noreply.github.com>
2020-12-17 17:19:21 -0600

    jamesjclark1 section 9 complete

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -17,19 +17,67 @@ namespace WorkingWithFiles
         // .Trim() might be useful in this situation.
         public static int WordCount(string fileName)
         {
-            return default;
-        }
+            var streamReader = new StreamReader(fileName);
+            var count = 0;
+            string[] words = null;
+            string sentence = null;
 
+            while (!streamReader.EndOfStream)
+            {
+                sentence = streamReader.ReadLine().Trim();
+                words = sentence.Split(' ');
+                count += words.Length;
+            }
+            return count;
+        }
         // 2- Write a method that reads a text file and returns the longest word in the file. Ex.
         // The file contains, "The dog named Maximus ran away from home." The method should return
         // "Maximus" the longest word contained in the file. Return "File is Empty" if the file is empty.
         // .Trim() might be useful in this situation.
         public static string LongestWord(string fileName)
         {
-            return default;
+            var streamReader = new StreamReader(fileName);
+            string[] words = null;
+            string sentence = null;
+            string longestWord = "File is Empty";
+
+            while (!streamReader.EndOfStream)
+            {
+                sentence = streamReader.ReadLine().Trim();
+                words = sentence.Split(' ');
+
+                var beginning = words[0];
+                for (var i = 0; i < words.Length; i++)
+                {
+
+
+                    if (words[i].Length > beginning.Length)
+                    {
+                        longestWord = words[i];
+
+                    }
+                    else
+                    {
+                        longestWord = beginning;
+                    }
+                }
+
+                string Longestword = beginning;
+
+                while (!streamReader.EndOfStream)
+                {
+                    sentence = streamReader.ReadLine().Trim();
+                    words = sentence.Split(' ');
+                    Longestword += words.Length;
+                    var i = 0;
+                    if (words[i].Length != beginning.Length)
+
+                    return words[i];
+                } 
+            }
+            return longestWord;
         }
     }
-
     public static class Program
     {
         public static void Main()
